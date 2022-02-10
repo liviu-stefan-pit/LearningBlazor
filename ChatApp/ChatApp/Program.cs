d13e261fb39d76227db744b62a61e6c93bfc8fb3
liviu-stefan-pit <liviustefan.petroaia@gmail.com>
2022-02-10 23:56:58 +0200

    simple chat app

---
+++ ChatApp/ChatApp/Program.cs
@@ -0,0 +1,41 @@
+using ChatApp.Data;
+using ChatApp.Hubs;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+//compress the data that is send between the server and the client to be faster
+builder.Services.AddResponseCompression(opts =>
+{
+    //for server and client to recognize the data type that is passed withthe request
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes
+        .Concat(new[] { "application/octet-stream" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapHub<ChatHub>("/chathub");
+app.MapFallbackToPage("/_Host");
+
+app.Run();
